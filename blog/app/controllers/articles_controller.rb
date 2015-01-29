77078e791fb359d983740981b2e1bf5711a849fd
skebix <nestor706@gmail.com>
2015-01-28 20:22:32 -0430

    CRUD ready.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,15 +1,15 @@
 class ArticlesController < ApplicationController
 
-  def new
-    @article = Article.new
-  end
-
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
   end
 
   def edit
@@ -34,6 +34,13 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
